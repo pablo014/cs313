257e14e36d5fc4e9cbb92e65c9f47bdcbc4e6b03
Angelo Pablo <angelopablo014@gmail.com>
2019-05-02 17:17:01 -0600

    Moved footer to php

--- web/footer.php
+++ web/footer.php
@@ -1,3 +1,3 @@
 <?php
-echo "<p>Contact Information:<br>Phone: (904) 662-4380<br>EMail: pab18001@byui.edu<br></p>"
+echo "<p>Contact Information:<br>Phone: (904) 662-4380<br>EMail: pab18001@byui.edu<br></p>";
 ?>
