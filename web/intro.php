396e807386e0c3a8be0a435b311f283708e77805
Angelo Pablo <angelopablo014@gmail.com>
2019-05-03 16:15:46 -0600

    Testing php

---
+++ web/intro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Angelo Pablo Homepage</title>
+    <link rel="stylesheet" href="intro.css">
+  </head>
+  <body>
+    <h1>Angelo Pablo</h1><br>
+    <div class="nav">
+      <a href="intro.html">Home</a>&nbsp; &nbsp;
+      <a href="assignments.html">Assignments</a> &nbsp; &nbsp; 
+      <a href="pun.html">My Puns</a> &nbsp; &nbsp;
+      <a href="https://en.wikipedia.org/wiki/Link_(The_Legend_of_Zelda)">A Link</a>
+      <a href="snap.php">Test</a>
+    </div>
+    <br>
+    <h2>About Me</h2><br>
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAP-rtAN3RypYVcYv3xH4oIhQ8z8cN3EW6yHnabDwZpnahO-CIAA" alt="Angelo Pablo"><br>
+      <p>
+	Hello! My name is Angelo Pablo. This Is currently my fourth semester in BYU-I. I am studying Software Engineering as a major and I am studying Chinese for fun. I enjoy running and swimming as it pertains to sports. My favorite thing to do is make puns and do magic tricks, but yeah that is my life.
+      </p>
+      <button type="button" onclick="snap.php">Snap!</button>
+    </div>
+    <br><br>
+
+    <footer>
+      <?php include "snap.php"; ?>
+    </footer>
+  </body>
+</html>
