ee1c43fa502ef0112bc88170493aef6121df043e
Angelo Pablo <angelopablo014@gmail.com>
2019-05-04 08:26:56 -0600

    change color of navbar of home page and created a selected tab

--- web/intro.php
+++ web/intro.php
@@ -3,13 +3,17 @@
   <head>
     <title>Angelo Pablo Homepage</title>
     <link rel="stylesheet" href="intro.css">
+    <style>
+      .class {
+      background-image: linear-gradient(lightblue, grey);
+      }
   </head>
   <body>
     <div class="head">
     <img src="https://i.pinimg.com/736x/15/b1/25/15b12565eb23e2804cccf0b0ed4b299e--pokemon-go-mystic.jpg" alt="Cool logo" height="150" width="125"><p>Angelo Pablo's Home Page</p>
     </div>
     <div class="nav">
-      <div><a href="intro.html">Home</a></div>
+      <div class="selected"><a href="intro.html">Home</a></div>
       <div><a href="assignments.html">Assignments</a></div>
       <div><a href="pun.html">My Puns</a></div>
       <div><a href="https://en.wikipedia.org/wiki/Link_(The_Legend_of_Zelda)">A Link</a></div>
