5f5923f7a74264a9837ac02c26098b4a9a94c019
Angelo Pablo <angelopablo014@gmail.com>
2019-05-04 08:03:40 -0600

    Adjusted layout of home page

--- web/intro.php
+++ web/intro.php
@@ -15,8 +15,8 @@
       <a href="https://en.wikipedia.org/wiki/Link_(The_Legend_of_Zelda)">A Link</a>
     </div>
     <br>
-    <h2>About Me</h2><br>
     <div class="body">
+      <h2>About Me</h2><br>
       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAP-rtAN3RypYVcYv3xH4oIhQ8z8cN3EW6yHnabDwZpnahO-CIAA" alt="Angelo Pablo"><br>
       <p>
 	Hello! My name is Angelo Pablo. This is currently my fourth semester in BYU-I. I am studying Software Engineering as a major and I am studying Chinese for fun. I enjoy running and swimming as it pertains to sports. My favorite thing to do is make puns and do magic tricks, but yeah that is my life.
