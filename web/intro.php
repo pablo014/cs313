6e619f23b366d348f2ddc593171db6ea7750e443
Angelo Pablo <angelopablo014@gmail.com>
2019-05-08 14:30:22 -0600

    Bug fixes

--- web/intro.php
+++ web/intro.php
@@ -21,8 +21,8 @@
       <img src="https://i.pinimg.com/736x/15/b1/25/15b12565eb23e2804cccf0b0ed4b299e--pokemon-go-mystic.jpg" alt="Cool logo" height="150" width="125"><p id="header" onmouseover="change()">Angelo Pablo's Home Page</p>
     </div>
     <div class="nav">
-      <div class="selected"><a href="intro.html">Home</a></div>
-      <div><a href="assignments.html">Assignments</a></div>
+      <div class="selected"><a href="intro.php">Home</a></div>
+      <div><a href="assignments.php">Assignments</a></div>
       <div><a href="pun.html">My Puns</a></div>
       <div><a href="https://en.wikipedia.org/wiki/Link_(The_Legend_of_Zelda)">A Link</a></div>
     </div>
