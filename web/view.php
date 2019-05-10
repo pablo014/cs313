662f01f7ffd80c826a636a21abd7d75edb2b9046
Angelo Pablo <angelopablo014@gmail.com>
2019-05-10 16:05:10 -0600

    changeable total

--- web/view.php
+++ web/view.php
@@ -8,15 +8,15 @@
       <?php 
 	 if(isset($_GET[plastic]))
 	 {
-         echo "<input type=checkbox value=40 name=plastic>"."Single Basket (Plastic): $"."$_GET[plastic]"."<br>"; 
+         echo "<input type=checkbox value=40 name=plastic checked>"."Single Basket (Plastic): $"."$_GET[plastic]"."<br>"; 
 	 }
 	 if(isset($_GET[metal]))
 	 {
-         echo "<input type=checkbox value=60 name=metal>"."Single Basket (Metal): $"."$_GET[metal]"."<br>";
+         echo "<input type=checkbox value=60 name=metal checked>"."Single Basket (Metal): $"."$_GET[metal]"."<br>";
 	 }
 	 if(isset($_GET[double]))
 	 {
-         echo "<input type=checkbox value=50 name=double>"."Double Basket: $"."$_GET[double]"."<br>";
+         echo "<input type=checkbox value=50 name=double checked>"."Double Basket: $"."$_GET[double]"."<br>";
 	 }
 	 
 	 echo "Total: "."$_GET[total]";
