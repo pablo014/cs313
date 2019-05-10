c084fc1209d934d160b20386c83a937c79b40990
Angelo Pablo <angelopablo014@gmail.com>
2019-05-10 14:08:32 -0600

    changeable total

--- web/view.php
+++ web/view.php
@@ -7,15 +7,15 @@
     <?php 
        if(isset($_GET[plastic]))
        {
-          echo "Single Basket (Plastic): "."$_GET[plastic]"; 
+          echo "Single Basket (Plastic): "."$_GET[plastic]"."<br>"; 
        }
        if(isset($_GET[metal]))
        {
-          echo "Single Basket (Metal: )"."$_GET[metal]";
+          echo "Single Basket (Metal: )"."$_GET[metal]"."<br>";
        }
        if(isset($_GET[double]))
        {
-          echo "Double Basket: "."$_GET[double]";
+          echo "Double Basket: "."$_GET[double]"."<br>";
        }
 
        echo "Total: $_GET[total]";
