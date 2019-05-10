3563b0ff20e4d7230a0beb95bf50ed208f55b42e
Angelo Pablo <angelopablo014@gmail.com>
2019-05-10 14:18:12 -0600

    changeable total

--- web/view.php
+++ web/view.php
@@ -4,21 +4,23 @@
     <title>View Cart</title>
   </head>
   <body>
+    <form action="browse.php" method="get">
     <?php 
        if(isset($_GET[plastic]))
        {
-          echo "Single Basket (Plastic): "."$_GET[plastic]"."<br>"; 
+          echo "<input type=checkbox value=40 name=plastic>"."Single Basket (Plastic): "."$_GET[plastic]"."<br>"; 
        }
        if(isset($_GET[metal]))
        {
-          echo "Single Basket (Metal: )"."$_GET[metal]"."<br>";
+          echo "<input type=checkbox value=60 name=metal>"."Single Basket (Metal): "."$_GET[metal]"."<br>";
        }
        if(isset($_GET[double]))
        {
-          echo "Double Basket: "."$_GET[double]"."<br>";
+          echo "<input type=checkbox value=50 name=double>"."Double Basket: "."$_GET[double]"."<br>";
        }
 
        echo "Total: $_GET[total]";
        ?>
+    </form>
   </body>
 </html>
