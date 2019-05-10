2af948254f99377cc194dbbe58276bf8371bc8e9
Angelo Pablo <angelopablo014@gmail.com>
2019-05-10 15:30:46 -0600

    changeable total

--- web/browse2.php
+++ web/browse2.php
@@ -10,7 +10,7 @@
       var total = <?php
          if(isset($_POST[plastic]))
          {
-         echo "Number($_POST[plastic], 10)";
+         echo "Number($_POST[plastic])";
 	    if(isset($_POSt[metal]) || isset($_POST[double]))
             {
             echo " + ";
@@ -18,7 +18,7 @@
          }
          if(isset($_POST[metal]))
          {
-         echo "Number($_POST[metal], 10)";
+         echo "Number($_POST[metal])";
 	    if(isset($_POST[double]))
 	    {
 	    echo " + ";
@@ -26,7 +26,7 @@
          }
          if(isset($_POST[double]))
          {
-         echo "Number($_POST[double], 10)";
+         echo "Number($_POST[double])";
          }
 
          ?>;
