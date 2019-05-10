65e56a8f1aa5c9f81133525dc718e58ea9138eeb
Angelo Pablo <angelopablo014@gmail.com>
2019-05-10 14:20:58 -0600

    changeable total

--- web/view.php
+++ web/view.php
@@ -4,23 +4,24 @@
     <title>View Cart</title>
   </head>
   <body>
-    <form action="browse.php" method="get">
-    <?php 
-       if(isset($_GET[plastic]))
-       {
-          echo "<input type=checkbox value=40 name=plastic>"."Single Basket (Plastic): "."$_GET[plastic]"."<br>"; 
-       }
-       if(isset($_GET[metal]))
-       {
-          echo "<input type=checkbox value=60 name=metal>"."Single Basket (Metal): "."$_GET[metal]"."<br>";
-       }
-       if(isset($_GET[double]))
-       {
-          echo "<input type=checkbox value=50 name=double>"."Double Basket: "."$_GET[double]"."<br>";
-       }
-
-       echo "Total: $_GET[total]";
-       ?>
+    <form action="browse.php" method="post">
+      <?php 
+	 if(isset($_GET[plastic]))
+	 {
+         echo "<input type=checkbox value=40 name=plastic>"."Single Basket (Plastic): "."$_GET[plastic]"."<br>"; 
+	 }
+	 if(isset($_GET[metal]))
+	 {
+         echo "<input type=checkbox value=60 name=metal>"."Single Basket (Metal): "."$_GET[metal]"."<br>";
+	 }
+	 if(isset($_GET[double]))
+	 {
+         echo "<input type=checkbox value=50 name=double>"."Double Basket: "."$_GET[double]"."<br>";
+	 }
+	 
+	 echo "Total: $_GET[total]";
+	 ?>
+      <button type="submit">Return</button>
     </form>
   </body>
 </html>
