c5e8ed6fc6eeaae7aa99cdd1cd3d7cf74252ca14
Angelo Pablo <angelopablo014@gmail.com>
2019-05-11 13:03:18 -0600

    Using sessions

--- web/view.php
+++ web/view.php
@@ -10,27 +10,33 @@
       <?php 
 	 if(isset($_GET[plastic]) || isset($_SESSION['plastic']))
 	 {
-            echo "<input type=checkbox value=40 id=plastic name=plastic onclick=plastic() checked>"."Single Basket (Plastic): $";
-	    if(isset($_GET[plastic]))
-	    {
-		echo $_GET[plastic]."<br>";
-	    }
-	    else
-	    {
-		echo $_SESSION['plastic'];
-	    }
-	    echo "<br>";
+            echo "<input type=checkbox value=40 id=plastic name=plastic onclick=plastic() checked>"."Single Basket (Plastic): $40<br>";
+	    $plastic = 40;
 	 }
-	 if(isset($_GET[metal]))
+	 else
 	 {
-         echo "<input type=checkbox value=60 id=metal name=metal checked>"."Single Basket (Metal): $"."$_GET[metal]"."<br>";
+	    $plastic = 0;
 	 }
-	 if(isset($_GET[double]))
+	 if(isset($_GET[metal]) || isset($_SESSION['metal']))
 	 {
-         echo "<input type=checkbox value=50 id=double name=double checked>"."Double Basket: $"."$_GET[double]"."<br>";
+         echo "<input type=checkbox value=60 id=metal name=metal checked>"."Single Basket (Metal): $60<br>";
+	 $metal = 60;
 	 }
-	 
-	 echo "Total: "."$_GET[total]";
+	 else
+	 {
+	 $metal = 0;
+	 }
+	 if(isset($_GET[double]) || isset($_SESSION['double']))
+	 {
+         echo "<input type=checkbox value=50 id=double name=double checked>"."Double Basket: $60<br>";
+	 $double = 60;
+	 }
+	 else
+	 {
+	 $double = 0;	
+	 }	 
+	 $total = $plastic + $metal + $double;
+	 echo "Total: $".$total;
 	 ?>
       <br><br>
       <button type="submit">Return</button><br><br>
