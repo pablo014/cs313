758c5fef10d556ac57ef5e15aab8f672a3f5ad02
Angelo Pablo <angelopablo014@gmail.com>
2019-05-11 12:24:47 -0600

    Using sessions

--- web/checkout.php
+++ web/checkout.php
@@ -7,7 +7,7 @@
     <h1>Check Out</h1>
     <form action="confirm.php" method="post">
       Address:<br>
-      <input type="textarea" name="address" rows="4" cols="50">
+      <textarea name="address" rows="4" cols="50"></textarea>
     </form>
   </body>
 </html>
