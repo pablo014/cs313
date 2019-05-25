2da0495d0b1c99449e16e344004ad09c3baf7afc
Angelo Pablo <angelopablo014@gmail.com>
2019-05-25 13:43:30 -0600

    Added display page of the prject

--- web/projList.php
+++ web/projList.php
@@ -24,7 +24,7 @@ catch (PDOException $ex)
 $rooms;
 foreach($db->query('SELECT * FROM room') as $row)
 {
-  echo 'Room '.$row['roomnum'].'<br>';
+  echo 'Room <a href=>'.$row['roomnum'].'</a><br><br>';
 }
 ?>
 <h1>17</h1>
