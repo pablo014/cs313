9a738dd206b67853c8fe58a65c17ea1fd1730164
Angelo Pablo <angelopablo014@gmail.com>
2019-05-25 10:09:43 -0600

    Added first page of the prject

--- web/projList.php
+++ web/projList.php
@@ -10,9 +10,9 @@ try
   $dbPort = $dbOpts["port"];
   $dbUser = $dbOpts["user"];
   $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/mydb');
+  $dbName = ltrim($dbOpts["path"],'/');
 
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=mydb", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
