35bdd1250a4c7e3ae28068ce57fb1f8e2f793c4d
Angelo Pablo <angelopablo014@gmail.com>
2019-05-28 18:31:46 -0600

    week 6 project

--- web/team06a.php
+++ web/team06a.php
@@ -14,12 +14,6 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
 $sql = "INSERT INTO Scripture(book, chapter, verse, content) VALUES (".$_POST['book'].", ".$_POST['chapter'].", ".$_POST['verse'].", ".$_POST['content'].")";
 $db->query($sql);
 
@@ -29,6 +23,18 @@ foreach($_POST["topic"] as $topic)
 {
 $db->query("INSERT INTO connect(topicId, scriptureId) VALUES ($topic, $newId)");
 }
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+catch (Exception $ex)
+{
+echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
 
 
 
