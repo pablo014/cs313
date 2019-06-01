21ddc43eb856c1316b22c4087b44b0355b8fd736
Angelo Pablo <angelopablo014@gmail.com>
2019-06-01 17:07:00 -0600

    Added remove student page for project

--- web/student.php
+++ web/student.php
@@ -30,11 +30,35 @@
    <?php 
       echo "<h1>Room ".$_SESSION["roomNumber"]."</h1>";   
    ?>
-   <div><a href="display.php">Home</a></div>
+   <div class="nav">   
+      <div><a href="display.php">Home</a></div>
       <div><a href="projList.php">Switch Apartment</a></div>
       <div><a href="grade.php">Grade Apartment</a></div>
       <div class="selected"><a href="addStudent.php">Add Student</a></div>
       <div><a href="removeStudent.php">Remove Student</a></div>
       <div><a href="student.php">Student View</a></div>
+   </div>
+   
+   <form action="student1.php" method="GET">
+   <select name="student">
+   <?php
+   foreach($db->query(SELECT * FROM Student) as $student)
+   {
+   echo "<option value=".$student['name'].">".$student['name']."</option>";
+   }
+   ?>
+   </select>
+   <br>
+   <select name="job">
+      <option value="Bathroom 1">Bathroom 1</option>
+      <option value="Bathroom 2">Bathroom 2</option>
+      <option value="Laundry Room">Laundry Room</option>
+      <option value="Kitchen Counter">Kitchen Counter</option>
+      <option value="Fridge">Fridge</option>
+      <option value="Stovetop">Stovetop</option>
+   </select>
+   <button type="submit">Submit</button>
+   </form>
+   
    </body>
 </html>
