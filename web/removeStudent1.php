e1de7c89729c74cecfb07dd0e329ebed38bd60bd
Angelo Pablo <angelopablo014@gmail.com>
2019-06-01 13:42:56 -0600

    Added remove student page for project

--- web/removeStudent1.php
+++ web/removeStudent1.php
@@ -20,6 +20,9 @@
    echo 'Error!: ' . $ex->getMessage();
    die();
    }      
-   echo $_GET["student"];
+   foreach($_GET["student"] as $student)
+   {
+      echo $student;
+   }
 ?>
 <br><a href="display.php">Return to Home</a>
