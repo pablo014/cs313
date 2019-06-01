42555ab4b2d5f0b50f2497c66cf5a3165a8a5835
Angelo Pablo <angelopablo014@gmail.com>
2019-06-01 16:26:02 -0600

    Added remove student page for project

---
+++ web/student.php
@@ -0,0 +1,40 @@
+<?php
+   session_start();
+   try
+   {
+   $dbUrl = getenv('DATABASE_URL');
+
+   $dbOpts = parse_url($dbUrl);
+
+   $dbHost = $dbOpts["host"];
+   $dbPort = $dbOpts["port"];
+   $dbUser = $dbOpts["user"];
+   $dbPassword = $dbOpts["pass"];
+   $dbName = ltrim($dbOpts["path"],'/');
+
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   }
+   catch (PDOException $ex)
+   {
+   echo 'Error!: ' . $ex->getMessage();
+   die();
+   }
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+   </head>
+   <body>
+   <?php 
+      echo "<h1>Room ".$_SESSION["roomNumber"]."</h1>";   
+   ?>
+   <div><a href="display.php">Home</a></div>
+      <div><a href="projList.php">Switch Apartment</a></div>
+      <div><a href="grade.php">Grade Apartment</a></div>
+      <div class="selected"><a href="addStudent.php">Add Student</a></div>
+      <div><a href="removeStudent.php">Remove Student</a></div>
+      <div><a href="student.php">Student View</a></div>
+   </body>
+</html>
