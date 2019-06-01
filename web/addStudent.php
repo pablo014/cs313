969422c340f966784990a9b2e99b400d400b439f
Angelo Pablo <angelopablo014@gmail.com>
2019-06-01 10:21:35 -0600

    Added add student page for project

--- web/addStudent.php
+++ web/addStudent.php
@@ -1,5 +1,59 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="proj.css">
+</head>
+<body>
 <?php
-   session_start();
    echo "<h1>Room ".$_SESSION["roomNumber"]."</h1>";
 ?>
-<p>hello</p>
+<?php
+        try
+        {
+        $dbUrl = getenv('DATABASE_URL');
+
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $ex)
+        {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+        }
+?>
+   <div class="nav">
+      <div><a href="display.php">Home</a></div>
+      <div><a href="projList.php">Switch Apartment</a></div>
+      <div><a href="">Grade Apartment</a></div>
+      <div class="selected"><a href="addStudent.php">Add Student</a></div>
+      <div><a href="">Remove Student</a></div>
+   </div>
+   
+   <form action="addStudent.php">
+      First Name: <br>
+      <input type="text" id="fname"><br>
+      Last Name: <br>
+      <input type="text" id="lname"><br><br>
+      <button type="submit" onclick="add()">Add Student</button>
+   </form>
+   <script>
+     function add(){
+     var name = document.getElementById("fname") + document.getElementById("lname")
+     <?php
+        $name = name;
+        $db->query('INSERT INTO Student(name) VALUES $name')
+     ?>
+     }
+  </script>
+</body>
+</html>
