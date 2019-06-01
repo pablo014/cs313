6cace117c3c7d2ed400a810bc4ec060b4dcf5bd6
Angelo Pablo <angelopablo014@gmail.com>
2019-06-01 12:53:50 -0600

    Added remove student page for project

--- web/removeStudent1.php
+++ web/removeStudent1.php
@@ -1,3 +1,25 @@
-<?php
-   echo "hello";
+<?php session_start();
+      try
+   {
+   $dbUrl = getenv('DATABASE_URL');
+
+   $dbOpts = parse_url($dbUrl);
+
+   $dbHost = $dbOpts["host"];
+   $dbPort = $dbOpts["port"];
+   $dbUser = $dbOpts["user"];
+   $dbPassword = $dbOpts["pass"];
+   $dbName = ltrim($dbOpts["path"],'/');
+
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   }
+   catch (PDOException $ex)
+   {
+   echo 'Error!: ' . $ex->getMessage();
+   die();
+   }      
+   echo $_GET["student"];
 ?>
+<br><a href="display.php">Return to Home</a>
