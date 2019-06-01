5524e40d0c8c5f49be5dfa2b5d7f9ae2afceb1ea
Angelo Pablo <angelopablo014@gmail.com>
2019-06-01 17:56:02 -0600

    Added remove student page for project

--- web/student1.php
+++ web/student1.php
@@ -1,2 +1,36 @@
 <?php
+   session_start();
+   try
+   {
+   $dbUrl = getenv('DATABASE_URL');
+
+   $dbOpts = parse_url($dbUrl);
+
+   $dbHost = $dbOpts["host"];
+   $dbPort = $dbOpts["port"];
+   $dbUser = $dbOpts["user"];
+   $dbPassword = $dbOpts["pass"];
+   $dbName = ltrim($dbOpts["path"],'/');
+
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   }
+   catch (PDOException $ex)
+   {
+   echo 'Error!: ' . $ex->getMessage();
+   die();
+   }
+   
+   $name = $_GET["name"];
+   $job = $_GET["job"];
+   
+   if($db->query('UPDATE Student SET job = $job WHERE name = $name'))
+   {
+      echo "Successfully Updated Job List";
+   }
+   else
+   {
+      echo "Error Unable to Update Job List"
+   }
 ?>
