536e5c896948aa4d3db3411351308933a6263bf2
Angelo Pablo <angelopablo014@gmail.com>
2019-06-01 09:23:00 -0600

    Added first page of the prject

--- web/display.php
+++ web/display.php
@@ -3,7 +3,40 @@
 <html>
 <body>
 <?php
+   $_SESSION["roomNumber"] = $_GET["rooms"];
    echo "<h1>Room ".$_GET["rooms"]."</h1>";
 ?>
+  <h2>Students</h2>
+  <?php
+     try
+     {
+	$dbUrl = getenv('DATABASE_URL');
+
+	$dbOpts = parse_url($dbUrl);
+
+  	$dbHost = $dbOpts["host"];
+  	$dbPort = $dbOpts["port"];
+  	$dbUser = $dbOpts["user"];
+  	$dbPassword = $dbOpts["pass"];
+  	$dbName = ltrim($dbOpts["path"],'/');
+
+  	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     }
+     catch (PDOException $ex)
+     {
+	echo 'Error!: ' . $ex->getMessage();
+     	die();
+     }
+     foreach($db->query('SELECT * FROM Student') as $row)
+     {
+	if($row['room'] == $_GET["rooms"])
+	{
+	   echo $row['name']." ".$row['job']."<br>";
+	}
+     }
+  ?>
+
 </body>
 </html>
