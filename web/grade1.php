664899dc8c60ce263399c7c31cf6a4247cc8add5
Angelo Pablo <angelopablo014@gmail.com>
2019-06-03 15:52:28 -0600

    Added remove student page for project

--- web/grade1.php
+++ web/grade1.php
@@ -1,2 +1,34 @@
 <?php
+   session_start();
+   try
+   {
+   $dbUrl = getenv('DATABASE_URL');
+
+   $dbOpts = parse_url($dbUrl);
+
+   $dbHost = $dbOpts["host"];
+   $dbPort = $dbOpts["port"];
+   $dbUser = $dbOpts["user"];
+   $dbPassword = $dbOpts["pass"];
+   $dbName = ltrim($dbOpts["path"],'/');
+
+   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   }
+   catch (PDOException $ex)
+   {
+   echo 'Error!: ' . $ex->getMessage();
+   die();
+   }
+   
+   $sql = "UPDATE Student SET comment='".$comment."', pass='".$pass."' WHERE name='".$name."'";
+   $i = 0;
+   foreach($db->query('SELECT * FROM Student') as $row)
+   {
+      if($row["room"] == $_SESSION["roomNumber"])
+      {
+         echo $row["name"]." ".$row["job"]." ".$_GET["comment[i]"];
+      }
+   }
 ?>
