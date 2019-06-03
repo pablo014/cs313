b283a61280e114db7a84a88252ddeed8a60fa60b
Angelo Pablo <angelopablo014@gmail.com>
2019-06-03 16:37:01 -0600

    Added remove student page for project

--- web/grade1.php
+++ web/grade1.php
@@ -43,13 +43,13 @@
 	 {
 	    $db->query("UPDATE Student SET pass = true WHERE name = '$name'");
 	    echo $array[$i];
+	    $i = $i + 1;
 	 }
 	 else
 	 {
 	    $db->query("UPDATE Student SET pass = false WHERE name = '$name'");
 	    echo $array[$i];
 	 }
-	 $i = $i + 1;
 
 	 $sql = "UPDATE Student SET comment = '$comment' WHERE name = '$name'";
 	 $db->query($sql);
