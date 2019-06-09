168c5c439d62ef17d9681c151cf690c566115944
Angelo Pablo <angelopablo014@gmail.com>
2019-06-08 19:40:14 -0600

    added a login page

--- db/myDb.sql
+++ db/myDb.sql
@@ -5,7 +5,7 @@ roomNum int
 
 CREATE TABLE student (
 studentId serial PRIMARY KEY,
-name varchar(50) NOT NULL,
+name text NOT NULL,
 job varchar(100) NOT NULL,
 pass boolean,
 room int REFERENCES room(roomId)
@@ -18,4 +18,31 @@ INSERT INTO room
 (roomId) VALUES	(3103);
 
 INSERT INTO student
-(name, job, pass, room) VALUES ('Angelo', 'Kitchen', TRUE, 3103);
+(name, job, pass, room) VALUES ('Angelo Pablo', 'Kitchen', TRUE, 3103);
+
+INSERT INTO student
+(name, job, pass, room) VALUES ('Tanner Larson', 'Living Room', TRUE, 3103);
+
+INSERT INTO student
+(name, job, pass, room) VALUES ('Jeff Richards', 'Stovetop', TRUE, 3103);
+
+INSERT INTO student
+(name, job, pass, room) VALUES ('Micah Lewellen', 'Living Room', TRUE, 3103);
+
+INSERT INTO student
+(name, job, pass, room) VALUES ('Warren Smith', 'Living Room', TRUE, 3103);
+
+INSERT INTO student
+(name, job, pass, room) VALUES ('Alex Williams', 'Living Room', TRUE, 3103);
+
+INSERT INTO room
+(roomId) VALUES (1102);
+
+INSERT INTO room
+(roomId) VALUES (3104);
+
+INSERT INTO room
+(roomId) VALUES (3105);
+
+INSERT INTO room
+(roomId) VALUES (3106);
